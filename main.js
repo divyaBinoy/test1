e5ebf339b9425085921c245d4740b95676dbed72
divyaBinoy <77156605+divyaBinoy@users.noreply.github.com>
2021-12-29 19:56:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function change_color(){
+    document.getElementById("heading1").style.color = "blue";
+}
+function change_color_black(){
+    document.getElementById("heading1").style.color = "black";
+}
